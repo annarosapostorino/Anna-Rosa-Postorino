362fe90627c06839a9817137a182ce58fc073b3f
annarosapostorino <quinogongora9@gmail.com>
2025-12-18 15:40:17 +0100

    Update index.js
    Ajuste móvil

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -1,1 +1,17 @@
-export { default } from './Brand';
+import React from 'react';
+import { navigate } from 'gatsby';
+import * as styles from './Brand.module.css'; // Importamos las reglas que acabas de crear
+
+const Brand = () => {
+  return (
+    <div
+      role="presentation"
+      onClick={() => navigate('/')}
+      className={styles.root} // Aquí está la magia: usa la clase .root del CSS
+    >
+      ANNA ROSA POSTORINO
+    </div>
+  );
+};
+
+export default Brand;
